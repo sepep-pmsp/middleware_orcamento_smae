8a2660895f637f9805af588c513c67e551b43fed
h-pgy <hpougy@gmail.com>
2022-12-02 14:49:30 -0300

    corrigindo error raise - .get nao da keyerror

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
+
 class MissingEnvironmentVariable(Exception):
     pass
 
 def get_my_env_var(var_name):
-    try:
-        envvar = os.environ.get(var_name)
-    except KeyError:
+
+    envvar = os.environ.get(var_name)
+    if envvar is None:
         raise MissingEnvironmentVariable(f"ENV '{var_name}' must be set")
     return envvar
 
 SOF_API_TOKEN = get_my_env_var('SOF_API_TOKEN')
-if not SOF_API_TOKEN:
-    raise MissingEnvironmentVariable(f"ENV '{var_name}' must not be empty")
