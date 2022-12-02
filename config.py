7ae159b1b79d7ccc4693af430bee4063d13296a6
h-pgy <hpougy@gmail.com>
2022-12-02 14:55:52 -0300

    using load dotenv to load env when running outside docker

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv 
 
 class MissingEnvironmentVariable(Exception):
     pass
 
 def get_my_env_var(var_name):
+
+    #carrega valores do .env para quando rodar fora do docker
+    load_dotenv()
 
     envvar = os.environ.get(var_name)
     if envvar is None:
